35c9f7a6866858acb721eaff9acfed1a7b110c67
samurailobster <jeffreymccharen@gmail.com>
2020-09-09 06:04:34 -0500

    Code refactor

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ const FAHRENHEIT_FREEZING_POINT : f64 = 32.0;
 
 fn main() {
 
+    loop{
     menu();
 
     let mut choice = String::new();
@@ -22,86 +23,76 @@ fn main() {
         Err(_) => 0,
     };
         
-        match choice {
-
-             2 => {
-
-                println!("Enter Fahrenheit temperature: ");
-
-                let mut f_temp = String::new();
-
-                io::stdin().read_line(&mut f_temp)
-                    .expect("Failed to read temp.");
-                
-                let f_temp : f64 = match f_temp.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => 0.0,
-                        };
-
-                convert_to_celsius(f_temp)
+    match choice {
+        1 => {
+            println!("Enter Celsius temperature: ");
+            let temp = get_temp();
+            convert_to_fahrenheit(temp);
             }
-
-
-            1 => {
-
-                println!("Enter Celsius temperature: ");
-
-                let mut c_temp = String::new();
-
-                io::stdin().read_line(&mut c_temp)
-                    .expect("Failed to read temp.");
-                
-                let c_temp : f64 = match c_temp.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => 0.0,
-                        };
-
-                convert_to_fahrenheit(c_temp)
+        2 => {
+            println!("Enter Fahrenheit temperature: ");
+            let temp = get_temp();
+            convert_to_celsius(temp);
             }
-
-            3  => {
+        3  => {
                 return
             }
-
-            _ => {
+        _ => {
                 return
             }
+}
 
-            }
+fn get_temp() -> f64 {
 
+    let mut temp = String::new();
 
-    fn menu() {
+    io::stdin().read_line(&mut temp)
+        .expect("Failed to read temp.");
+    
+    let temp : f64 = match temp.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0.0,
+            };
+    
+    temp
+}
+
+fn menu() {
         println!("Convert to Fahrenheit -> Press 1");
         println!("Convert to Celsius -> Press 2");
         println!("Exit program -> Press 3");
         println!("Enter choice: ");
     }
         
-    fn convert_to_fahrenheit(temp : f64) {
-        let f_temp  = (temp * 1.8) + 32.0;
-        println!("{} degrees Celsius converts to {} degrees Fahrenheit.", temp, f_temp);
-        check_temps(f_temp: f64);
+fn convert_to_fahrenheit(temp : f64) {
+    let f_temp  = (temp * 1.8) + 32.0;
+    println!("{} °C converts to {} °F.", temp, f_temp);
+    check_temps(f_temp: f64);
     }
 
-    fn convert_to_celsius(temp : f64) {
-        let c_temp = (temp - 32.0) * 0.5556;
-        println!("{} degrees Fahrenheit converts to {} degrees Celsius.", temp, c_temp);
-        check_temps(c_temp :f64);
+fn convert_to_celsius(temp : f64) {
+    let c_temp = (temp - 32.0) * 0.5556;
+    println!("{} °F converts to {} °C.", temp, c_temp);
+    check_temps(c_temp :f64);
     }
 
-    fn check_temps(temp : f64) {
-       if temp == CELSIUS_BOILING_POINT {
-            println!("{} degrees Celsius is the boiling point of water!", temp);
+fn check_temps(temp : f64) {
+    if temp == CELSIUS_BOILING_POINT {
+        println!("{} °C is the boiling point of water!", temp);
         }
-        if temp == FAHRENHEIT_BOILING_POINT {
-            println!("{} degrees Fahrenheit is the boiling point of water!", temp);
+    if temp == FAHRENHEIT_BOILING_POINT {
+        println!("{} °F is the boiling point of water!", temp);
         }
-        if temp == CELSIUS_FREEZING_POINT {
-            println!("{} degrees Celsius is the freezing point of water!", temp);
+    if temp == CELSIUS_FREEZING_POINT {
+        println!("{} °C is the freezing point of water!", temp);
         }
-        if temp == FAHRENHEIT_FREEZING_POINT {
-            println!("{} degrees Fahrenheit is the freezing point of water!", temp);
+    if temp == FAHRENHEIT_FREEZING_POINT {
+        println!("{} °F is the freezing point of water!", temp);
+        }
+    if temp >= (300.0 * CELSIUS_BOILING_POINT) {
+        println!("{} °C is equal to the temperature of lightning!", temp);
         }
     }
 
+}
 }
